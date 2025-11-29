1a8bd0b4817be1c3e459dc7767f6d919ffdfc8c7
Budi Kurniawan <budi2020@gmail.com>
2025-11-29 22:25:07 +1100

    Added plot_util

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,51 +1,17 @@
 import gymnasium as gym
-import matplotlib
-import matplotlib.pyplot as plt
 import random
 from itertools import count
 
 import torch
 
 from agent.dqn2.dqn_agent import DQNAgent
+from util.plot_util import plot_timesteps
 import os
 import time
 
-
-"""
-Setting amsgrad=True in the AdamW optimizer makes the results reproducible and better.
-"""
 os.system('cls' if os.name == 'nt' else 'clear')
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-plt.ion()
 
-def plot_durations(episode_durations, show_result=False):
-    plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
-    # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
-
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
-
-def train_dqn(env, dqn_agent, num_episodes: int, seed: int, device, draw_chart: bool = False):
+def train_dqn(env, dqn_agent, num_episodes: int, seed: int, device):
     episode_durations = []
 
     for i_episode in range(num_episodes):
@@ -58,19 +24,12 @@ def train_dqn(env, dqn_agent, num_episodes: int, seed: int, device, draw_chart: 
             if terminated or truncated:
                 episode_durations.append(t + 1)
                 print("episode ", i_episode, ", reward: ", t + 1)
-                if draw_chart:
-                    plot_durations(episode_durations)
                 break
             state = next_state
-    if draw_chart:
-        plot_durations(episode_durations, True)
-        plt.ioff()
-        plt.show()
     return episode_durations      
 
 
 if __name__ == "__main__":
-    # if GPU is to be used
     device = torch.device(
         "cuda" if torch.cuda.is_available() else
         "mps" if torch.backends.mps.is_available() else
@@ -89,16 +48,16 @@ if __name__ == "__main__":
     n_actions = env.action_space.n
     state, info = env.reset()
     n_observations = len(state) #4
-    num_episodes = 500
+    num_episodes = 50
     dqn_agent = DQNAgent(n_observations, n_actions, env, device)
-    draw_chart = False
     
 
     print("device:", device)
     start = time.time()
-
-    results = train_dqn(env, dqn_agent, num_episodes, seed, device, draw_chart)
-
+    results = train_dqn(env, dqn_agent, num_episodes, seed, device)
     end = time.time()
     print("Total rewards:", sum(results))
     print(f"Execution time: {end - start:.4f} seconds")
+    draw_chart = False
+    if draw_chart:
+        plot_timesteps(results, True)
